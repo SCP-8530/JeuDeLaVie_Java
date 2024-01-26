9d5ca15a2ee7fb244381af26952961bb18cee120
Guillaume Paoli <2142678@cegepoutaouais.qc.ca>
2024-01-26 10:32:33 -0500

    Afficher un tableau

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -3,25 +3,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameOfLife {
-    private static List<int[]> CelluleActif;
-    private static List<int[]> FuturCelluleActif;
+    private static Boolean[][] TableauPresent;
+    private static Boolean[][] TableauFutur;
 
-    public Array ChangeEtat(int [] cellule) {
+    public static void main(String[] args) {
+        //creer un tableau
+        TableauPresent = new Boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, false, false}
+        };
+
+        //afficher le tableau
+        AfficherTableau(TableauPresent);
+    }
+
+    public static void AfficherTableau(Boolean[][] tab) {
+        for(int i1 = 0; i1 < tab.length; i1++){
+            for (int i2 = 0; i2 < tab.length; i2++) {
+                if (tab[i1][i2]) { System.out.print("#");}
+                else {System.out.print(".");}
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static int[] ChangeEtat(int[] cellule) {
         int x = cellule[0];
         int y = cellule[1];
         int etat = cellule[2];
         int nombre = 0;
 
-        //Regarde les
-        if (CelluleActif.contains(new int[]{x - 1, y + 1, 1})) nombre++;
-        if (CelluleActif.contains(new int[]{x, y + 1, 1})) nombre++;
-        if (CelluleActif.contains(new int[]{x + 1, y + 1, 1})) nombre++;
+        //Regarde les cellules autours
+        if (TableauPresent[x-1][y+1]) nombre++;
+        if (TableauPresent[x][y+1]) nombre++;
+        if (TableauPresent[x+1][y+1]) nombre++;
 
-        if (CelluleActif.contains(new int[]{x - 1, y, 1})) nombre++;
-        if (CelluleActif.contains(new int[]{x + 1, y, 1})) nombre++;
+        if (TableauPresent[x-1][y]) nombre++;
+        if (TableauPresent[x+1][y]) nombre++;
 
-        if (CelluleActif.contains(new int[]{x + 1, y + 1, 1})) nombre++;
-        if (CelluleActif.contains(new int[]{x + 1, y + 1, 1})) nombre++;
-        if (CelluleActif.contains(new int[]{x + 1, y + 1, 1})) nombre++;
+        if (TableauPresent[x-1][y-1]) nombre++;
+        if (TableauPresent[x][y-1]) nombre++;
+        if (TableauPresent[x+1][y-1]) nombre++;
+
+        //retourne la cellule
+        switch (nombre) {
+            case 2: return new int[]{x, y, etat};
+            case 3: return new int[]{x, y, 1};
+            default: return new int[]{x, y, 0};
+        }
     }
 }
