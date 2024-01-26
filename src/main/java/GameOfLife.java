26dd50e38e4e5e0feb9f44d00121490924f6aa49
Guillaume Paoli <2142678@cegepoutaouais.qc.ca>
2024-01-26 11:00:20 -0500

    [WIP] Changement du type du table (bool to int)

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -3,25 +3,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameOfLife {
-    private static Boolean[][] TableauPresent;
-    private static Boolean[][] TableauFutur;
+    private static Integer[][] TableauDepart = new Integer[][]{
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+
+    };
+    private static Integer[][] TableauPresent;
+    private static Integer[][] TableauFutur;
 
     public static void main(String[] args) {
         //creer un tableau
-        TableauPresent = new Boolean[][]{
-                {false, false, false},
-                {false, false, false},
-                {false, false, false}
-        };
+        TableauPresent = TableauDepart;
 
         //afficher le tableau
         AfficherTableau(TableauPresent);
     }
 
-    public static void AfficherTableau(Boolean[][] tab) {
+    public static void AfficherTableau(Integer[][] tab) {
         for(int i1 = 0; i1 < tab.length; i1++){
             for (int i2 = 0; i2 < tab.length; i2++) {
-                if (tab[i1][i2]) { System.out.print("#");}
+                if (tab[i1][i2] == 1) { System.out.print("#");}
                 else {System.out.print(".");}
             }
             System.out.print("\n");
@@ -35,16 +55,16 @@ public class GameOfLife {
         int nombre = 0;
 
         //Regarde les cellules autours
-        if (TableauPresent[x-1][y+1]) nombre++;
-        if (TableauPresent[x][y+1]) nombre++;
-        if (TableauPresent[x+1][y+1]) nombre++;
+        if (TableauPresent[x-1][y+1] == 1) nombre++;
+        if (TableauPresent[x][y+1] == 1) nombre++;
+        if (TableauPresent[x+1][y+1] == 1) nombre++;
 
-        if (TableauPresent[x-1][y]) nombre++;
-        if (TableauPresent[x+1][y]) nombre++;
+        if (TableauPresent[x-1][y] == 1) nombre++;
+        if (TableauPresent[x+1][y] == 1) nombre++;
 
-        if (TableauPresent[x-1][y-1]) nombre++;
-        if (TableauPresent[x][y-1]) nombre++;
-        if (TableauPresent[x+1][y-1]) nombre++;
+        if (TableauPresent[x-1][y-1] == 1) nombre++;
+        if (TableauPresent[x][y-1] == 1) nombre++;
+        if (TableauPresent[x+1][y-1] == 1) nombre++;
 
         //retourne la cellule
         switch (nombre) {
